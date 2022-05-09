c59ca459981a93437af875c0f5bf041da6fc5b2d
aegislsy <leesaeyim@naver.com>
2022-05-09 23:48:24 +0900

    0509

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,30 +1,22 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
+  const [toDo, setTodo] = useState("");
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => event.preventDefault();
+
+  console.log(toDo);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write tyour to do..."
+        />
+        <button> Add To do</button>
+      </form>
     </div>
   );
 }
